a70771f8daf7e21fe6e5142d64688ff09502bff7
Xploitable <dannychaindog@gmail.com>
2024-02-19 19:40:32 +0400

    set middleware auth for accessing api methods

---
+++ package/handler/middleware.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	AUTHORIZATION_HEADER = "Authorization"
+	USER_CTX             = "userId"
+)
+
+func (h *Handler) userIdentity(c *gin.Context) {
+	header := c.GetHeader(AUTHORIZATION_HEADER)
+	if header == "" {
+		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
+		return
+	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+
+	userId, err := h.services.Authorization.ParseToken(headerParts[1])
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	c.Set(USER_CTX, userId)
+
+}
--- package/service/auth.go
+++ package/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
 	"time"
 
@@ -54,4 +55,22 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 	})
 
 	return token.SignedString([]byte(signin_key))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid sign-in method")
+		}
+		return []byte(viper.GetString("app.signinkey")), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*TokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *TokenClaims")
+	}
+	return claims.UserId, nil
 }
